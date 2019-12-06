cfd3d96432cae2e145caee16f53c08de4c2b3686
luckaskill <daniil.kovelkov@gmail.com>
2019-12-06 21:58:58 +0300

    MI-0 //crypt test fix

--- src/main/test/CryptTest.java
+++ src/main/test/CryptTest.java
@@ -7,8 +7,13 @@ public class CryptTest {
     void crypt() {
         String value = "Crypt";
         byte[] bytes = {1, 2, 3, 4, 5, 9};
-        String encrypt = Cryptor.encrypt(value, bytes);
-        String decoded = Cryptor.decrypt(encrypt, bytes);
-        Assertions.assertEquals(value, decoded);
+        String encrypt1 = Cryptor.encrypt(value, bytes);
+        String encrypt2 = Cryptor.encrypt(value, bytes);
+        Assertions.assertNotEquals(encrypt1, encrypt2);
+
+        String decoded1 = Cryptor.decrypt(encrypt1, bytes);
+        String decoded2 = Cryptor.decrypt(encrypt2, bytes);
+        Assertions.assertEquals(value, decoded1);
+        Assertions.assertEquals(value, decoded2);
     }
 }
